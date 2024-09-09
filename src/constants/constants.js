556fa982942ba9c34e87d3f57ea2fd77ea61e761
Blessingkeyz <umukoroblessing@gmail.com>
2024-09-10 00:38:53 +0100

    New entry

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const projects = [
     id: 0,
   },
   {
-    title: "E-Commerce",
+    title: "Teetree",
     description:
       "Successfully created and worked on the Sign Up, Login ,Support, Statistics and Settings pages of a website called Teetree. I also made sure the web pages were responsive and lag free to give a full user experience.Also collaborated with other front end developers through GIT and the UI/UX designers in the team to make the final web app ready.",
     image: "/images/2.png",
@@ -18,26 +18,6 @@ export const projects = [
     source: "https://google.com",
     visit: "https://google.com",
     id: 1,
-  },
-  {
-    title: "WebRTC App",
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/images/3.jpg",
-    tags: ["React", "WebRTC"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 2,
-  },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
   },
 ];
 
@@ -55,8 +35,9 @@ export const TimeLineData = [
     year: 2022,
     text: "Built various web apps and collaborated with remote developers",
   },
-  { year: 2022, text: "Worked as a freelance developer" },
+  { year: 2023, text: "Started with Angular and Typescript" },
+  { year: 2024, text: "Working as a freelance developer" },
 ];
 export const Aboutme = {
-  text: "I am a Junior Web Developer ready to build in all stages of advanced web development. Knowledgeable in user interface creation processes. Proficient in an assortment of technologies, including React, Version Control System, CSS, and JavaScript. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.",
+  text: "I am a Web Developer knowledgeable in user interface creation processes. Proficient in an assortment of technologies, including React, NextJs, Version Control System, Tailwind CSS, and JavaScript. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.",
 };
